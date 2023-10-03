3cc9be9d9f499d30fc253119136ece4f9164fcb9
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-03 20:19:28 +0100

    code fail fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-const getListStudents = () => {
+export default function getListStudents() {
   const arr = [
     { id: 1, firstName: 'Guillame', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
@@ -6,6 +6,4 @@ const getListStudents = () => {
   ];
 
   return arr;
-};
-
-export default getListStudents;
+}

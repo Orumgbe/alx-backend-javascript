68f0826af36403c68a0159e6895571fc7dd8ff36
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-03 19:59:01 +0100

    fix requirements

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
   const arr = [];
   const obj1 = {
-    "id": 1,
-    "firstName": "Guillame",
-    "location": "San Francisco"
+    id: 1,
+    firstName: 'Guillame',
+    location: 'San Francisco',
   };
-  const obj2 =  {
-    "id": 2,
-    "firstName": "James",
-    "location": "Columbia"
+  const obj2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
   };
-  const obj3 =  {
-    "id": 5,
-    "firstName": "Serena",
-    "location": "San Francisco"
+  const obj3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
   };
 
   arr.push(obj1, obj2, obj3);

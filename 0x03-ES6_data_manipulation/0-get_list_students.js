e8577c56d8482e5930c1a64ad324cae390834e69
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-03 19:44:37 +0100

    array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const arr = [];
+  const obj1 = {
+    "id": 1,
+    "firstName": "Guillame",
+    "location": "San Francisco"
+  };
+  const obj2 =  {
+    "id": 2,
+    "firstName": "James",
+    "location": "Columbia"
+  };
+  const obj3 =  {
+    "id": 5,
+    "firstName": "Serena",
+    "location": "San Francisco"
+  };
+  
+  arr.push(obj1, obj2, obj3);
+  return arr;
+}

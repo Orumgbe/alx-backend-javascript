02c54fd3c4ab3f22aac8e5a3f8ba3953ae87df4f
Orumgbe <orumgbeugbekile@gmail.com>
2024-04-30 08:59:15 -0700

    basic setup of nodejs http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+// Importing http module
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  // Set reponse http header with HTTP status code and content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Send response body
+  res.end('Hello Holberton School!');
+});
+
+// Set listening port
+app.listen(1245)
+
+// Export server
+module.exports = app;

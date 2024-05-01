c70c2082632b3327758bad66d955f6561ad08d87
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-01 12:02:07 -0700

    fix lint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,13 +3,13 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   // Set reponse http header with HTTP status code and content type
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   // Send response body
   res.end('Hello Holberton School!');
 });
 
 // Set listening port
-app.listen(1245)
+app.listen(1245);
 
 // Export server
 module.exports = app;

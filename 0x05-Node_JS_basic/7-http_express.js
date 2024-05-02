7d96ecd312970266a893eef55d9c4498a59d6b2a
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-02 02:53:28 -0700

    expressjs server

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,33 @@
+// Import required modules and create express instance
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+// Retrieve database name from command line
+const [,, DATABASE] = process.argv;
+
+// Set listening port
+app.listen(1245);
+
+// Root url
+app.get('/', (req, res) => {
+  res.contentType('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+// Students url
+app.get('/students', async (req, res) => {
+  res.contentType('text/plain');
+  try {
+    const students = await countStudents(DATABASE);
+    let nugget = 'This is the list of our students\n';
+    nugget += students.join('\n');
+    res.send(nugget);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+// Export app
+module.exports = app;

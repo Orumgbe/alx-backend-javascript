74d4e60be7fae794d8fc23e9a477834ab9b89a64
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-27 06:12:28 -0700

    handling multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const upload = await uploadPhoto();
-    const value = await createUser();
-    console.log(upload.body, value.firstName, value.lastName);
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName, lastName } = values[1];
+
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }

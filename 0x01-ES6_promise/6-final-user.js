ed55eedfd2a8e7f53f8f097da9cbb44dcec8ab4c
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-27 07:20:20 -0700

    test

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const user = await signUpUser(firstName, lastName);
-  const photo = await uploadPhoto(fileName);
-  return ([{ status: (user.status, photo.status), value: (user, photo) }]);
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

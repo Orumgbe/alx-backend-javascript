c9ae9e662e9a1b502b46c05d0bc1bf39cc81950d
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-28 01:03:24 -0700

    fix output

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -2,9 +2,13 @@ export default class Currency {
   constructor(code, name) {
     if (typeof code === 'string') {
       this._code = code;
+    } else {
+      throw TypeError('Code must be a string');
     }
     if (typeof name === 'string') {
       this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
     }
   }
 

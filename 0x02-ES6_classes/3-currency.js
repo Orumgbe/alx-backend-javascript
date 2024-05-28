232ac403b92b45053ee987e3c4fc0bb8589cba7e
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-28 00:45:19 -0700

    currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof (code) === 'string') {
+      this._code = code;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} ${this._code}`;
+  }
+}

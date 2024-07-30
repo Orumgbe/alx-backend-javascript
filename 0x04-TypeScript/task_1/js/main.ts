8de619559149c236a167b916dcd5c6303e840908
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-30 09:41:02 +0100

    added student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,32 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number
 }
+
+interface printTeacherFunction{
+    (firstName: string, lastName: string): string
+}
+
+const printTeacher:printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string):StudentClassInterface
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string
+  displayName(): string
+}
+
+class StudentClass implements StudentClassInterface{
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
